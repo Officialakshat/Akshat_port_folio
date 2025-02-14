e3208edee88cd168b54d4be2983ef9cfac4ec488
Akshat <officialakshat044@gmail.com>
2025-02-14 20:43:41 +0530

    update

--- js/script.js
+++ js/script.js
@@ -84,19 +84,19 @@ const nav = document.querySelector(".nav"),
           }
 
            // --------------------------- Js for google sheet submit button in contact section----------------
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbzlyAjoxXd1zUvFscLNWtKzWeDIeSEm3rOqxM4eC7dYPItAYEi3QxjrIe4oDogr5MP1/exec'
-  const form = document.forms['submit-to-google-sheet']
-  const msg = document.getElementById('msg')
+//     const scriptURL = 'https://script.google.com/macros/s/AKfycbzlyAjoxXd1zUvFscLNWtKzWeDIeSEm3rOqxM4eC7dYPItAYEi3QxjrIe4oDogr5MP1/exec'
+//   const form = document.forms['submit-to-google-sheet']
+//   const msg = document.getElementById('msg')
 
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => {
-        msg.innerHTML = "Message Sent Successfully"
-        setTimeout(function(){
-            msg.innerHTML = ""
-        }, 5000)
-        form.reset()
-      })
-      .catch(error => console.error('Error!', error.message))
-  })
+//   form.addEventListener('submit', e => {
+//     e.preventDefault()
+//     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+//       .then(response => {
+//         msg.innerHTML = "Message Sent Successfully"
+//         setTimeout(function(){
+//             msg.innerHTML = ""
+//         }, 5000)
+//         form.reset()
+//       })
+//       .catch(error => console.error('Error!', error.message))
+//   })
